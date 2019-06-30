cb154660ea7aa24adf14c01cd759a3e3d1b66c50
Adam Piascik <adam.piascik@gmail.com>
2019-06-30 20:04:02 +0100

    created jsontest.ps1 for testing download and parsing of swagger doc

---
+++ jsontest.ps1
@@ -0,0 +1,20 @@
+﻿$version = 1
+$baseurl = "https://horizonleeds-api.dev.avamae.co.uk"
+$parametersFile = "parameters.json"
+$auths = ""
+
+$response = Invoke-RestMethod -Uri ($baseurl + "/v" + $version.ToString() + "/doc.json")
+
+for ($auth in $auths) {
+
+}
+
+$response."paths".PSObject.Properties | ForEach-Object {
+    $endpoint = $_.Name
+    $_.Value.PSObject.Properties | ForEach-Object {
+        $method = $_.Name
+        $_.Value.PSObject.Properties | ForEach-Object {
+            $
+        }
+    }
+}

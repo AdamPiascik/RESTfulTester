1067298a0e16f4ac5b185e561b32c9f9c23b124f
Adam Piascik <adam.piascik@avamae.co.uk>
2019-07-01 09:29:22 +0100

    added function blocks and code for Get-EndpointParameters

--- jsontest.ps1
+++ jsontest.ps1
@@ -1,20 +1,48 @@
-﻿$version = 1
+﻿function Get-EndpointParameters {
+    param ( $endpoint,
+            $parametersFile )
+
+    process {
+        $parameters = @{}
+        if ($parametersFile) {
+            $jsonData = Get-Content $parametersFile | ConvertFrom-Json
+            $jsonData.$endpoint.PSObject.Properties | ForEach-Object {
+            $parameters.Add($_.Name, $_.Value)
+            }
+        }
+        return $parameters
+    }
+}
+
+function Get-AllAccessTokens {
+
+}
+
+function Get-AccessTokenForEndpoint {
+
+}
+
+
+
+$version = 1
 $baseurl = "https://horizonleeds-api.dev.avamae.co.uk"
 $parametersFile = "parameters.json"
 $auths = ""
 
-$response = Invoke-RestMethod -Uri ($baseurl + "/v" + $version.ToString() + "/doc.json")
+$swaggerDoc = Invoke-RestMethod -Uri ($baseurl + "/v" + $version.ToString() + "/doc.json")
 
-for ($auth in $auths) {
-
+$accesstokens = @{}
+foreach ($auth in $auths) {
+    $response = Invoke-RestMethod -Uri ($baseurl + $auth)
 }
 
-$response."paths".PSObject.Properties | ForEach-Object {
+[System.Collections.ArrayList] $endpointsList = @()
+
+$swaggerDoc."paths".PSObject.Properties | ForEach-Object {
     $endpoint = $_.Name
     $_.Value.PSObject.Properties | ForEach-Object {
         $method = $_.Name
-        $_.Value.PSObject.Properties | ForEach-Object {
-            $
-        }
+        $endpointsList.Add(@($endpoint, $method, "`n")) > $null
+        Write-Host( $endpointsList)
     }
 }
